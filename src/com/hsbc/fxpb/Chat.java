f6d32eb372fbdb1e55be67c60f98b006e47c229a
vincentjzeng <Vz1546500>
2013-08-30 21:24:19 +0100

    Init project

---
+++ src/com/hsbc/fxpb/Chat.java
@@ -0,0 +1,14 @@
+package com.hsbc.fxpb;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: think
+ * Date: 13-8-30
+ * Time: 下午9:19
+ * To change this template use File | Settings | File Templates.
+ */
+public class Chat {
+    public static void main(String[] args){
+        System.out.println("hi");
+    }
+}
